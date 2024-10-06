2b0e023443cf1b9ec5e1b631d5be375b7dead2db
AyushKumarShaw <webdev0912@gmail.com>
2024-10-06 21:30:20 +0530

    made changes regarding logout

--- api/routes/userRoutes.js
+++ api/routes/userRoutes.js
@@ -6,7 +6,7 @@ const router=express.Router()
 
 router.post("/register",register)
 router.post("/login",login)
-router.post("/logout",logout)
+router.post("/logout",isAuthenticated,logout)
 router.get("/authcheck",isAuthenticated,authCheck)
 
 export default router
