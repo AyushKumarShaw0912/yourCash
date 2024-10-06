7b2ac9e67aaa444f27194780d0f8e460d703ea47
AyushKumarShaw <webdev0912@gmail.com>
2024-10-06 20:17:22 +0530

    changes made regarding token

--- api/middlewares/auth.js
+++ api/middlewares/auth.js
@@ -5,9 +5,10 @@ import { User } from "../models/userModel.js";
 import { catchAsyncHandler } from "./catchAsyncHandler.js";
 
 
-export const isAuthenticated=catchAsyncHandler(async(req,res,next)=>{
+export const isAuthenticated=catchAsyncHandler(async(req,res,next,err)=>{
     const {token}=req.cookies;
-    if(!token) return next(new ErrorHandler("Invalid user token",404))
+    console.log(token)
+    if(!token) return next(new ErrorHandler(err.message,404))
 
     const decoded=jwt.verify(token,process.env.JWT_SECRET)
    
