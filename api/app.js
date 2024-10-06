13220e7895353a4b82ac89f1f228abae93ee754a
AyushKumarShaw <webdev0912@gmail.com>
2024-10-06 19:06:45 +0530

    first commit

--- api/app.js
+++ api/app.js
@@ -2,6 +2,8 @@ import express from "express"
 import cors from "cors"
 import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
+import path from "path"
+import ErrorMiddleware from "./middlewares/error.js"
 
 
 
@@ -26,6 +28,15 @@ import categoryRouter from "./routes/categoryRoutes.js"
 app.use("/api/v1",userRoutes)
 app.use("/api/v1/expense",expenseRoutes)
 app.use("/api/v1/category",categoryRouter)
+const __dirname = path.resolve();
+
+
+	app.use(express.static(path.join(__dirname, "/client/dist")));
+
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"));
+	});
 
 
 export default app
+app.use(ErrorMiddleware)
